64bf82f7ec6e6d969ad329744d79f13ab66b6681
Donavon Cai <donavoncai@yahoo.com>
2022-05-12 16:20:57 -0700

    extracted components from registration form

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -17,39 +17,44 @@ export function LoginForm() {
 
   return (
     <div id="login-form-container">
-      <Typography id="modal-title"
-                  variant="h5"
-                  component="h2"
-                  align="center">
+      <Typography
+          id="modal-title"
+          variant="h5"
+          component="h2"
+          align="center">
         Login
       </Typography>
 
-      <Typography id="modal-instructions"
-                  color="#616161"
-                  align="center">
+      <Typography
+          id="modal-instructions"
+          color="#616161"
+          align="center">
         Please enter your username and password.
       </Typography>
 
       <FormControl align="center">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField  label="Username"
-                      size="small"
-                      variant="outlined"
-                      sx={{ marginTop: '8px' }}
-                      required>
+          <TextField
+              label="Username"
+              size="small"
+              variant="outlined"
+              sx={{ marginTop: '8px' }}
+              required>
           </TextField>
 
-          <TextField  label="Password"
-                      size="small"
-                      variant="outlined"
-                      sx={{ marginTop: '8px' }}
-                      type="password"
-                      required>
+          <TextField
+              label="Password"
+              size="small"
+              variant="outlined"
+              sx={{ marginTop: '8px' }}
+              type="password"
+              required>
           </TextField>
 
-          <Button type="submit"
-                  variant="contained"
-                  sx={{ display: 'block', marginTop: '8px' }}>
+          <Button
+              type="submit"
+              variant="contained"
+              sx={{ display: 'block', marginTop: '8px' }}>
             Log in
           </Button>
         </form>
--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,22 +1,14 @@
 import axios from 'axios'
 import { useState } from 'react'
-import { Button, TextField, Typography, FormControl }  from '@mui/material'
+import { Button, Typography, FormControl }  from '@mui/material'
 import { useForm } from 'react-hook-form'
-import { ErrorMessage } from '@hookform/error-message'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { StatusCodes } from 'http-status-codes'
+import { RegisterTextField, RegisterPasswordField } from './RegisterInputFields'
 import * as Yup from 'yup'
 import './RegisterForm.css'
 
-import { Visibility, VisibilityOff } from '@mui/icons-material'
-
 export function RegisterForm() {
-  // Constants:
-  const textFieldProps = {
-    size: "small",
-    variant: "outlined",
-    sx: { marginTop: '8px' },
-  };
-
   // Form validation schema:
   const validationSchema = Yup.object().shape({
     username: Yup.string()
@@ -37,35 +29,9 @@ export function RegisterForm() {
     mode: "onBlur",
     resolver: yupResolver(validationSchema)
   });
-  const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
+  const [enableButton, setEnableButton] = useState(true);
 
   // Helper functions:
-  const togglePassword = () => {
-    setShowPassword(!showPassword);
-  }
-
-  const togglePasswordConfirm = () => {
-    setShowPasswordConfirm(!showPasswordConfirm);
-  }
-
-  const checkAvailability = (fieldName) => {
-    const fieldData = getValues(fieldName);
-
-    if (!fieldData) return;
-
-    axios.get('register', {params: {field: fieldName, data: fieldData}})
-    .then((res) => {
-      console.log(res);
-      let fieldExists = res.data;
-      if (fieldExists)
-        setError(fieldName, {type: "custom", message: "This " + fieldName + " is already taken."});
-    })
-    .catch((error) => {
-      console.error(error.response.data);
-    })
-  }
-
   const onSubmit = (data) => {
     const {username, email, password} = data;
     axios.post('register', {
@@ -74,10 +40,13 @@ export function RegisterForm() {
       password: password,
     })
     .then((res) => {
-      console.log(res.status);
+      if (res.status === StatusCodes.OK) {
+        setEnableButton(false);
+        // TODO: log user in?
+      }
     })
     .catch((error) => {
-      if (error.response.status === 409) { // conflict
+      if (error.response.status === StatusCodes.CONFLICT) {
         const field = error.response.data.field;
         if (field === 'username') {
           setError('username', {type: "custom", message: "This username is already taken."});
@@ -108,69 +77,41 @@ export function RegisterForm() {
 
       <FormControl align='center'>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField
-              label='Username'
-              {...register('username', {
-                onBlur: (e) => {checkAvailability('username')}
-              })}
-              {...textFieldProps}>
-          </TextField>
-          <ErrorMessage
-              errors={errors}
+          <RegisterTextField
               name='username'
-              as={<p className='error-message' />}>
-          </ErrorMessage>
-
-          <TextField
-              label='Email'
-              type='email'
-              {...register('email', {
-                onBlur: (e) => {checkAvailability('email')}
-              })}
-              {...textFieldProps}>
-          </TextField>
-          <ErrorMessage
+              displayName='Username'
+              register={register}
+              getValues={getValues}
               errors={errors}
+              setError={setError}>
+          </RegisterTextField>
+
+          <RegisterTextField
               name='email'
-              as={<p className='error-message' />}>
-          </ErrorMessage>
-
-          <TextField
-              label='Password'
-              type={showPassword? 'text' : 'password'}
-              {...register('password')}
-              {...textFieldProps}
-              InputProps={{endAdornment:
-                            <div class='eye' onClick={togglePassword}>
-                              {showPassword? <Visibility /> : <VisibilityOff />}
-                            </div>
-              }}>
-          </TextField>
-          <ErrorMessage
+              displayName='Email'
+              register={register}
+              getValues={getValues}
               errors={errors}
+              setError={setError}>
+          </RegisterTextField>
+
+          <RegisterPasswordField
               name='password'
-              as={<p className='error-message' />}>
-          </ErrorMessage>
+              displayName='Password'
+              register={register}
+              errors={errors}>
+          </RegisterPasswordField>
 
-          <TextField
-              label='Password Confirmation'
-              type={showPasswordConfirm? 'text' : 'password'}
-              {...register('confirmPassword')}
-              {...textFieldProps}
-              InputProps={{endAdornment:
-                            <div class='eye' onClick={togglePasswordConfirm}>
-                              {showPasswordConfirm? <Visibility /> : <VisibilityOff />}
-                            </div>
-              }}>
-          </TextField>
-          <ErrorMessage
-              errors={errors}
+          <RegisterPasswordField
               name='confirmPassword'
-              as={<p className='error-message' />}>
-          </ErrorMessage>
+              displayName='Password Confirmation'
+              register={register}
+              errors={errors}>
+          </RegisterPasswordField>
 
           <Button
               type='submit'
+              disabled={!enableButton}
               variant='contained'
               sx={{ display: 'block', marginTop: '8px' }}>
             Sign Up
